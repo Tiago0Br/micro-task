83fe2e0a738975bef0f4b116b0bd62bf1ad6770f
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-01-15 18:57:33 -0300

    refactor: Utilizando as validações do Dto e removendo arquivos desnecessários

--- apps/notifications-service/src/app.module.ts
+++ apps/notifications-service/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 
 @Module({
   imports: [
@@ -20,7 +18,7 @@ import { AppService } from './app.service'
       })
     })
   ],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
--- apps/tasks-service/src/tasks/tasks.controller.ts
+++ apps/tasks-service/src/tasks/tasks.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import type { LoggedUser } from 'src/auth/jwt.strategy'
+import { ValidationPipe } from 'src/http/validation.pipe'
 import { CreateTaskDto } from './dtos/create-task.dto'
 import { TasksService } from './tasks.service'
 
@@ -11,7 +12,10 @@ export class TasksController {
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
-  create(@Body() body: CreateTaskDto, @CurrentUser() user: LoggedUser) {
+  create(
+    @Body(new ValidationPipe()) body: CreateTaskDto,
+    @CurrentUser() user: LoggedUser
+  ) {
     const { userId } = user
     return this.tasksService.create(body, userId)
   }
