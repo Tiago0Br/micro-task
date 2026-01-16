0f24866bbf84f103f1ff669d6b41638f82c7abeb
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-01-16 17:57:38 -0300

    config: configurando o Tanstack Router na aplicação web

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App } from './app'
+
+import { routeTree } from './routeTree.gen'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
